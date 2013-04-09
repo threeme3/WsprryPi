3ad19978abd851a2ccdc51b18dd628d1732932b2
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2013-04-09 22:19:00 +0200

    tune mode

--- wspr.c
+++ wspr.c
@@ -569,7 +569,14 @@ int main(int argc, char *argv[])
     if(band >= nbands)
       band = 0;
     if(centre_freq) setupDMATab(centre_freq, 1.0/wspr_symtime, wspr_symtime, 4);
-    if (tune_mode == 0) wait_every((wspr15) ? 15 : 2);
+    if (tune_mode == 1) 
+	 {
+		 printf ("Tune mode");
+	 }
+	 else
+	 {
+		 wait_every((wspr15) ? 15 : 2);
+	 }
     time_t t;
     time(&t);
     char buf[256];
