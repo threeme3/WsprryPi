47e4421e7a55992a73b64b8404a13af4214bc4fe
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2013-04-14 10:29:45 +0200

    wip daemon integration

--- wspr.c
+++ wspr.c
@@ -38,10 +38,14 @@ License:
 #include <sys/stat.h>
 #include <signal.h>
 #include <malloc.h>
+#include <errno.h>
+#include <pwd.h>
 #include <time.h>
 
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
+#define DAEMON_NAME "wspr"
+#define RUN_AS_USER "root" 
 
 #define F_XTAL       (19229581.050215044276577479844352)             // calibrated 19.2MHz XTAL frequency 
 #define F_PLLD_CLK   (26.0 * F_XTAL)                                 // 500MHz PLLD reference clock 
@@ -531,24 +535,76 @@ void wait_every(int minute)
 }
 
 // http://www-theorie.physik.unizh.ch/~dpotter/howto/daemonize
-static void daemonize(void)
+static void child_handler(int signum)
 {
-    pid_t pid, sid;
+    switch(signum) {
+    case SIGALRM: exit(EXIT_FAILURE); break;
+    case SIGUSR1: exit(EXIT_SUCCESS); break;
+    case SIGCHLD: exit(EXIT_FAILURE); break;
+    }
+}
+
+// http://www-theorie.physik.unizh.ch/~dpotter/howto/daemonize
+static void daemonize( const char *lockfile )
+{
+    pid_t pid, sid, parent;
+    int lfp = -1;
 
     /* already a daemon */
     if ( getppid() == 1 ) return;
 
+    /* Create the lock file as the current user */
+    if ( lockfile && lockfile[0] ) {
+        lfp = open(lockfile,O_RDWR|O_CREAT,0640);
+        if ( lfp < 0 ) {
+            syslog( LOG_ERR, "unable to create lock file %s, code=%d (%s)",
+                    lockfile, errno, strerror(errno) );
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    /* Drop user if there is one, and we were run as root */
+    if ( getuid() == 0 || geteuid() == 0 ) {
+        struct passwd *pw = getpwnam(RUN_AS_USER);
+        if ( pw ) {
+            syslog( LOG_NOTICE, "setting user to " RUN_AS_USER );
+            setuid( pw->pw_uid );
+        }
+    }
+
+    /* Trap signals that we expect to recieve */
+    signal(SIGCHLD,child_handler);
+    signal(SIGUSR1,child_handler);
+    signal(SIGALRM,child_handler);
+
     /* Fork off the parent process */
     pid = fork();
     if (pid < 0) {
+        syslog( LOG_ERR, "unable to fork daemon, code=%d (%s)",
+                errno, strerror(errno) );
         exit(EXIT_FAILURE);
     }
     /* If we got a good PID, then we can exit the parent process. */
     if (pid > 0) {
-        exit(EXIT_SUCCESS);
+
+        /* Wait for confirmation from the child via SIGTERM or SIGCHLD, or
+           for two seconds to elapse (SIGALRM).  pause() should not return. */
+        alarm(2);
+        pause();
+
+        exit(EXIT_FAILURE);
     }
 
     /* At this point we are executing as the child process */
+    parent = getppid();
+
+    /* Cancel certain signals */
+    signal(SIGCHLD,SIG_DFL); /* A child process dies */
+    signal(SIGTSTP,SIG_IGN); /* Various TTY signals */
+    signal(SIGTTOU,SIG_IGN);
+    signal(SIGTTIN,SIG_IGN);
+    signal(SIGHUP, SIG_IGN); /* Ignore hangup signal */
+    signal(SIGTERM,SIG_DFL); /* Die on SIGTERM */
 
     /* Change the file mode mask */
     umask(0);
@@ -556,12 +612,16 @@ static void daemonize(void)
     /* Create a new SID for the child process */
     sid = setsid();
     if (sid < 0) {
+        syslog( LOG_ERR, "unable to create a new session, code %d (%s)",
+                errno, strerror(errno) );
         exit(EXIT_FAILURE);
     }
 
     /* Change the current working directory.  This prevents the current
        directory from being locked; hence not being able to remove it. */
     if ((chdir("/")) < 0) {
+        syslog( LOG_ERR, "unable to change directory to %s, code %d (%s)",
+                "/", errno, strerror(errno) );
         exit(EXIT_FAILURE);
     }
 
@@ -569,7 +629,12 @@ static void daemonize(void)
     freopen( "/dev/null", "r", stdin);
     freopen( "/dev/null", "w", stdout);
     freopen( "/dev/null", "w", stderr);
+
+    /* Tell the parent process that we are A-okay */
+    kill( parent, SIGUSR1 );
 }
+
+
 
 int main(int argc, char *argv[])
 {
@@ -582,6 +647,11 @@ int main(int argc, char *argv[])
   int band = 0;
   int tune_mode = 0;
 
+  /* Initialize the logging interface */
+  openlog( DAEMON_NAME, LOG_PID, LOG_LOCAL5 );
+  syslog( LOG_INFO, "starting" );
+
+  /* Commandline Stuff */
   if(argc < 5){
     printf("Usage: wspr <[prefix/]callsign[/A-Z,/0-9,/00-99]> <locator> <power in dBm> [<frequency in Hz or 0 for interval> ...]\n");
     printf("\te.g.: sudo ./wspr K1JT/P JO21 10 7040074 0 0 10140174 0 0\n");
@@ -607,7 +677,8 @@ int main(int argc, char *argv[])
   txon();
   setupDMA();
   printf("Ready for transmit...\n");
-  
+
+  /* Daemonize */
   daemonize();
 
   for(;;)
@@ -640,6 +711,10 @@ int main(int argc, char *argv[])
       } 
     }
   }
+
+  /* Finish up */
+  syslog( LOG_NOTICE, "terminated" )
+  closelog();
 
   return 0;
 }
