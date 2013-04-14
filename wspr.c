8203676f1a37af0920e16ce8b4cd414d3d7d3db1
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2013-04-14 10:04:32 +0200

    daemon

--- wspr.c
+++ wspr.c
@@ -40,6 +40,9 @@ License:
 #include <malloc.h>
 #include <time.h>
 
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
 #define F_XTAL       (19229581.050215044276577479844352)             // calibrated 19.2MHz XTAL frequency 
 #define F_PLLD_CLK   (26.0 * F_XTAL)                                 // 500MHz PLLD reference clock 
 
@@ -527,6 +530,46 @@ void wait_every(int minute)
   usleep(T_SECOND); // wait another second
 }
 
+static void daemonize(void)
+{
+    pid_t pid, sid;
+
+    /* already a daemon */
+    if ( getppid() == 1 ) return;
+
+    /* Fork off the parent process */
+    pid = fork();
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    /* If we got a good PID, then we can exit the parent process. */
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+
+    /* At this point we are executing as the child process */
+
+    /* Change the file mode mask */
+    umask(0);
+
+    /* Create a new SID for the child process */
+    sid = setsid();
+    if (sid < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Change the current working directory.  This prevents the current
+       directory from being locked; hence not being able to remove it. */
+    if ((chdir("/")) < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Redirect standard files to /dev/null */
+    freopen( "/dev/null", "r", stdin);
+    freopen( "/dev/null", "w", stdout);
+    freopen( "/dev/null", "w", stderr);
+}
+
 int main(int argc, char *argv[])
 {
   unsigned char symbols[162];
@@ -563,6 +606,8 @@ int main(int argc, char *argv[])
   txon();
   setupDMA();
   printf("Ready for transmit...\n");
+  
+  daemonize();
 
   for(;;)
   {
